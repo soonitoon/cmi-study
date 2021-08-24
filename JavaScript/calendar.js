ea98ffd2d51ebb3066b46a4f28ad8919cc5cdb44
Hyuno Choi <soonitoon@gmail.com>
2021-08-25 00:52:37 +0900

    Feat: Set default value of date (#1)

--- JavaScript/calendar.js
+++ JavaScript/calendar.js
@@ -1,3 +1,15 @@
 const yearContainer = document.querySelector(".year");
 const monthContainer = document.querySelector(".month");
 const dateContainer = document.querySelector(".date");
+
+const dateInstance = new Date();
+
+let year = dateInstance.getFullYear();
+let month = dateInstance.getMonth() + 1;
+let date = dateInstance.getDate();
+
+let dateArray = [];
+
+for (let i = 1; i < 32; i++) {
+  dateArray.push(i);
+}
