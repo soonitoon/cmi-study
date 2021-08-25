33e827e21bc284f3b540b8cdd7a9752f95d48489
Hyuno Choi <soonitoon@gmail.com>
2021-08-25 22:50:20 +0900

    Feat: Make last date of calendar be changed automatically (#1)

--- JavaScript/calendar.js
+++ JavaScript/calendar.js
@@ -4,17 +4,20 @@ const dateContainer = document.querySelector(".date_list");
 
 const dateInstance = new Date();
 
+const lastDateOfMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 let year = dateInstance.getFullYear();
-let month = dateInstance.getMonth() + 1;
+let month = dateInstance.getMonth();
 let date = dateInstance.getDate();
 
 const setCurrentDate = () => {
   yearContainer.innerText = year;
-  monthContainer.innerText = month;
+  monthContainer.innerText = month + 1;
 };
 
 const drawDateTemplate = () => {
-  for (let i = 1; i < 32; i++) {
+  const lastDate = lastDateOfMonth[month];
+  for (let i = 1; i <= lastDate; i++) {
     const eachDate = document.createElement("li");
     eachDate.innerText = i;
     dateContainer.appendChild(eachDate);
